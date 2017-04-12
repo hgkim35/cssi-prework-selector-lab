8a8708d00f09f3235dd509c183d2a8f74885a269
hgkim35 <hgkim@google.com>
2017-04-12 13:50:52 -0400

    selector lab complete

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,22 @@ function pSelector(){
   return $('p');
 }
 */
+function h1Selector(){
+  return $('h1');
+}
+
+function liIn01Selector(){
+  return $('ol il');
+}
+
+function linkSelector(){
+  return $('a #my-id .my-class');
+}
+
+function catSelector(){
+  return $('alt = "cat sleeping":image');
+}
+
+function checkboxInputSelector(){
+  return $(':checkbox');
+}
